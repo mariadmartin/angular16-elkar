53f9687e377e0b71f43a4f0c5b3f092c22cd738c
Pilar Martin <mariadmartin@gmail.com>
2024-03-28 12:35:55 +0100

    Event binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'product-list',
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  client: string = "John Doe";
+  // client: string = "John Doe";
+  addToCart: number = 0;
   product = {
     name: "iphone",
     price: 99.99,
@@ -18,6 +19,25 @@ export class ProductListComponent {
 
   getDiscountPrice() {
     return this.product.price -(this.product.price * this.product.discount/100)
+  }
+
+  onChangeName(event: any) {
+
+    //this.client = event.target.value;
+    console.log(event.target.value);
+  }
+
+  decrementCartValue() {
+    if (this.addToCart > 0) {
+      this.addToCart--;
+    }
+  }
+
+  incrementCartValue() {
+    if (this.addToCart< this.product.inStock){
+      this.addToCart++;
+    }
+
   }
 
 }
