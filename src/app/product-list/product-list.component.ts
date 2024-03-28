fc4d9733213cda8c54c5c6e933d6d02328545686
Pilar Martin <mariadmartin@gmail.com>
2024-03-28 18:33:45 +0100

    ngFor Main menu

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,38 +6,6 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  // client: string = "John Doe";
-  addToCart: number = 0;
-  product = {
-    name: "iphone",
-    price: 99.99,
-    color: "Matte Black",
-    discount: 10,
-    inStock: 5,
-    image: '/assets/images/iphone.png'
-  }
-
-  getDiscountPrice() {
-    return this.product.price -(this.product.price * this.product.discount/100)
-  }
-
-  onChangeName(event: any) {
-
-    //this.client = event.target.value;
-    console.log(event.target.value);
-  }
-
-  decrementCartValue() {
-    if (this.addToCart > 0) {
-      this.addToCart--;
-    }
-  }
-
-  incrementCartValue() {
-    if (this.addToCart< this.product.inStock){
-      this.addToCart++;
-    }
-
-  }
+  listOfString: string[] = ['Pilar', 'Jon', 'Isidro', 'Rolly'];
 
 }
