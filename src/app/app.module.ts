1d37119274fcbced0325ffdf30dd046cf696bf43
Pilar Martin <mariadmartin@gmail.com>
2024-03-28 14:22:27 +0100

    Two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { TopheaderComponent } from './topheader/topheader.component';
 import { TopMenuComponent } from './header/top-menu/top-menu.component';
 import { MainMenuComponent } from './header/main-menu/main-menu.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { SearchComponent } from './product-list/search/search.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,10 +18,12 @@ import { ProductListComponent } from './product-list/product-list.component';
     TopheaderComponent,
     TopMenuComponent,
     MainMenuComponent,
-    ProductListComponent
+    ProductListComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
