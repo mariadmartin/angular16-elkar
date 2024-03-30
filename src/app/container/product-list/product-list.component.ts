414492bd6610f6c0fa3fa77b1ad897032fbdafda
Pilar Martin <mariadmartin@gmail.com>
2024-03-30 19:51:34 +0100

    @Input pass data to child component from parent component

--- src/app/container/product-list/product-list.component.ts
+++ src/app/container/product-list/product-list.component.ts
@@ -19,7 +19,8 @@ export class ProductListComponent {
       price: 160,
       discountPrice: 0,
       is_in_invertory: true,
-      imageUrl: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/cf45e4d8-ecdf-4d1a-899f-98c32b34bcbb/air-jordan-1-low-zapatillas-QnthX1.png" },
+      items_left: 0,
+      imageUrl: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/cf45e4d8-ecdf-4d1a-899f-98c32b34bcbb/air-jordan-1-low-zapatillas-QnthX1.png"},
     {
       id: 2,
       name: "Nike React Infinity Run Flyknit",
@@ -32,8 +33,8 @@ export class ProductListComponent {
       price: 160,
       discountPrice: 90,
       is_in_invertory: true,
-      imageUrl: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/cf45e4d8-ecdf-4d1a-899f-98c32b34bcbb/air-jordan-1-low-zapatillas-QnthX1.png",
-      slug: "nike-react-infinity-run-flyknit"
+      items_left: 0,
+      imageUrl: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/cf45e4d8-ecdf-4d1a-899f-98c32b34bcbb/air-jordan-1-low-zapatillas-QnthX1.png"
     },
     {
       id: 3,
@@ -47,8 +48,9 @@ export class ProductListComponent {
       price: 160,
       discountPrice: 125,
       is_in_invertory: false,
-      imageUrl: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/cf45e4d8-ecdf-4d1a-899f-98c32b34bcbb/air-jordan-1-low-zapatillas-QnthX1.png",
-      slug: "nike-react-infinity-run-flyknit"
+      items_left: 0,
+      imageUrl: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/cf45e4d8-ecdf-4d1a-899f-98c32b34bcbb/air-jordan-1-low-zapatillas-QnthX1.png"
+
     }
   ]
 
---
+++ src/app/container/product-list/product/product.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+  @Input()
+  product: {
+      id: number,
+      name: string,
+      description: string,
+      brand: string,
+      gender: string,
+      category:string,
+      size: number[],
+      color: string[],
+      price: number,
+      discountPrice: number,
+      is_in_invertory: boolean,
+      items_left: number,
+      imageUrl: string
+  };
+
+  productDisconuntPrice(prod: any) {
+    return ((prod.discountPrice / prod.price * 100 )).toFixed(0);
+  }
+
+}
